a8b480214532d918a0615377a965190d31f14ef6
Shukla247 <diveshkumarshukla@gmail.com>
2023-07-26 20:04:54 +0530

    Learn Array

---
+++ arrays.js
@@ -0,0 +1,71 @@
+const nums = [23, 6, 5, 34, 57, 62, '8674'];
+
+console.log(nums);
+console.log(typeof nums);
+
+const movies = ['Batman', 'Barbie', 'Oppenheimer','Inception','Conjuring', 'Gader'];
+
+console.log(movies.length);
+console.log(movies[3]);
+console.log(movies[-2]);
+
+console.log(movies.at(-2));
+
+// Slicing
+
+console.log(movies.slice(2, 5));
+console.log(movies.slice(2, -1));
+console.log(movies.slice(2));
+console.log(movies.slice(2, 100));
+
+// adding Eliments
+
+movies.push('PK');   // adds element at end of array
+
+movies.unshift('Flash');  //adds element at start of array
+
+console.log(movies);  
+
+// removing elements
+
+movies.pop();   // removes element from end of array
+movies.shift(); //remove element from start of array
+
+console.log(movies);
+
+movies.splice(3, 2);
+console.log(movies);
+
+movies.splice(1, 1, 'Frozen');
+
+console.log(movies);
+
+movies.splice(2, 3);
+
+console.log(movies);
+
+console.log(...movies);  //... is sprit operator  
+
+console.log(['mi4', ...movies, 'mi1', 'mi2', 'mi3']);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
